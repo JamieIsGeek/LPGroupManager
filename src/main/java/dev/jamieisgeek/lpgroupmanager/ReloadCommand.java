41061f49b7de94eb775959c0e935455d7d487b1f
JamieIsGeek <jamieisgeek@gmail.com>
2022-06-11 17:59:15 +0100

    q

--- src/main/java/dev/jamieisgeek/lpgroupmanager/ReloadCommand.java
+++ src/main/java/dev/jamieisgeek/lpgroupmanager/ReloadCommand.java
@@ -6,8 +6,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class ReloadCommand implements CommandExecutor {
+    Plugin plugin = LPGroupManager.getProvidingPlugin(LPGroupManager.class);
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
@@ -16,8 +18,8 @@ public class ReloadCommand implements CommandExecutor {
             Player p = (Player) sender;
             if(p.hasPermission("lpgroupmanager.reload")) {
                 p.sendMessage(ChatColor.GREEN + "Reloading...");
-                Bukkit.getPluginManager().disablePlugin(LPGroupManager.getPlugin());
-                Bukkit.getPluginManager().enablePlugin(LPGroupManager.getPlugin());
+                Bukkit.getPluginManager().disablePlugin(plugin);
+                Bukkit.getPluginManager().enablePlugin(plugin);
                 p.sendMessage(ChatColor.GREEN + "Reloaded!");
             } else {
                 p.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
